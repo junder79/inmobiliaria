d793affc407c5baf0f6dc1ac1e0b7eb2cb27d4ca
javiieerhuerta <javieer.h17@gmail.com>
2022-08-26 15:44:21 -0400

    Update conexion.php

--- APP/conexion.php
+++ APP/conexion.php
@@ -4,9 +4,9 @@
         function  conectarBD() {
             //Conexion a la base de datos 
             $host="localhost";
-            $user="grupohexxa_inmobiliaria";
-            $password="aE,q=mEz~oIa";
-            $database="grupohex_inmobiliariaapp";
+            $user="grupohexxacom_inmobiliaria";
+            $password="Hl*uRbDkh[@Z";
+            $database="grupohexxacom_inmobiliaria";
     
             $conexion =mysqli_connect($host , $user , $password , $database) or die ("Error al conectar");
              mysqli_set_charset($conexion, 'utf8');
